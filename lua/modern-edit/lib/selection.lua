52e55495328a4122cea6221fcb3e279d8bd50a5b
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-10 09:55:27 +0900

    fix: fix problem

--- lua/modern-edit/lib/selection.lua
+++ lua/modern-edit/lib/selection.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup(opts)
-    vim.api.nvim_set_keymap('i', '<C-a>', '<C-o>vgg0<Esc><C-o>vG$', opts)
+    vim.api.nvim_set_keymap('i', '<C-a>', '<C-o>vgg0<Esc><C-o>vG$', { noremap = true })
 end
 
 return M
